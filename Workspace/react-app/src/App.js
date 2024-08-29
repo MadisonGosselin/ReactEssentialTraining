89101abab090e000e39e4a071324112688bd103e
Madison Gosselin <madisoncgosselin@gmail.com>
2024-08-29 13:11:55 -0400

    4.07

--- Workspace/react-app/src/App.js
+++ Workspace/react-app/src/App.js
@@ -1,26 +1,17 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useReducer } from "react";
 
 function App() {
-    const [emotion, setEmotion] = useState("happy");
-    const [secondary, setSecondary] = useState("tired");
-
-    useEffect(() => {
-        console.log(`Its ${emotion} right now`);
-    }, [emotion]);
-
-    useEffect(() => {
-        console.log(`Its ${secondary} around here`);
-    }, [secondary]);
+    const [checked, setChecked] = useReducer((checked) => !checked, false);
 
     return (
         <div className="App">
-            <h1>Current emotion is {emotion}</h1>
-            <button onClick={() => setEmotion("sad")}>Sad</button>
-            <button onClick={() => setEmotion("excited")}>Excited</button>
-
-            <h2>Current secondary emotion is {secondary}</h2>
-            <button onClick={() => setSecondary("grateful")}>Grateful</button>
+            <input
+                type="checkbox"
+                value={checked}
+                onChange={setChecked}
+            ></input>
+            <label>{checked ? "checked" : "not checked"}</label>
         </div>
     );
 }
