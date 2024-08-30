37b3adcc03ed4a1762859dfed0c0ecaac5bf9a51
Madison Gosselin <madisoncgosselin@gmail.com>
2024-08-30 07:54:07 -0400

    8.04

--- Workspace/react-app/src/Star.test.js
+++ Workspace/react-app/src/Star.test.js
@@ -3,6 +3,7 @@ import { Star } from "./Star";
 
 test("renders an h1", () => {
     const { getByText } = render(<Star />);
+    // eslint-disable-next-line testing-library/prefer-screen-queries
     const h1 = getByText(/Cool Star/);
     expect(h1).toHaveTextContent("Cool Star");
 });
